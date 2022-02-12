9004f84efb97bc2e18afeb6bc1ceae8dfc58a895
Dvornick <mail@dvornick.ru>
2022-02-12 16:46:54 +0300

    Work with API

---
+++ src/components/ErrorIndicator/ErrorIndicator.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './ErrorIndicator.css'
+import icon from './DeathStar.png'
+
+const ErrorIndicator = () => {
+    return (
+        <div className='text-warning text-center mx-auto'>
+            <img className='mb-2' src={icon} alt='Error icon: Death Star'></img>
+            <div className='lead'>BOOM!</div>
+            <span>Something has gone terribly wrong</span>
+        </div>
+    )
+}
+
+export default ErrorIndicator
